8534476fa9e95e718b35801ea97a0c6063c033d5
Anand Patel <48526944+anand1221178@users.noreply.github.com>
2026-06-10 10:07:37 +0200

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        int left = 0; //IDX
+
+
+        for(int right = 1; right < prices.size(); ++right)
+        {
+
+            int curr_profit =0;
+            curr_profit = prices[right] - prices[left];
+            if(prices[right]< prices[left])
+            {
+                left = right;
+            }
+
+            if(curr_profit > max_profit)
+            {
+                max_profit = curr_profit;
+            }
+
+        }
+
+        return max_profit;
+    }
+};
