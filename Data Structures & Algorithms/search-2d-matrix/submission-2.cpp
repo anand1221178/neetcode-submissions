787fe2f38c51895273c99a605e5b926ceef6c01a
Anand Patel <48526944+anand1221178@users.noreply.github.com>
2026-06-11 18:12:34 +0200

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m  = matrix.size(); // rows
+        int n = matrix[0].size(); //cols
+
+        // Check which row:
+        for(int i =0 ; i<m; i++)
+        {
+            if((target >= matrix[i][0]) && (target<=matrix[i][n-1]))
+            {
+                // Do bst to find the element:
+                int l=0;
+                int r = n-1;
+
+                while(l<=r)
+                {
+                    int mpt = l+(r-l)/2;
+
+                    if(target == matrix[i][mpt])
+                    {
+                        return true;
+                    }
+                    else if(matrix[i][mpt] < target)
+                    {
+                        l = mpt+1;
+                    }
+                    else
+                    {
+                        r = mpt-1;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
