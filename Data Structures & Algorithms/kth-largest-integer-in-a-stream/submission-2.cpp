2ceb17fffa115281acc799911404547b5315e466
Anand Patel <48526944+anand1221178@users.noreply.github.com>
2026-06-17 11:27:15 +0200

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.cpp
@@ -0,0 +1,34 @@
+class KthLargest {
+private: 
+    priority_queue<int , vector<int>, greater<int>> min_heap;
+    int cap;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        cap = k;
+        for(int i =0 ; i<nums.size(); i++)
+        {
+            if(min_heap.size() < cap)
+            {
+                min_heap.push(nums[i]);
+            }
+            else if(nums[i] > min_heap.top())
+            {
+                min_heap.pop();
+                min_heap.push(nums[i]);
+            }
+        }
+    }
+    
+    int add(int val) {
+        if(min_heap.size() < cap)
+        {
+            min_heap.push(val);
+        }
+        else if(val > min_heap.top())
+        {
+            min_heap.pop();
+            min_heap.push(val);
+        }
+        return min_heap.top();
+    }
+};
